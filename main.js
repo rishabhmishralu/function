dc78c665088dad1c4e2bd9924b0aa1b686073171
= <=>
2022-12-15 11:42:08 +0530

    add

---
+++ main.js
@@ -0,0 +1,80 @@
+
+
+
+// function fun(){
+
+//     console.log("function is here");
+
+// }
+
+
+
+// 2
+// function sum(a,b){
+//     console.log(a+b); 
+// }
+// sum(3,4)
+
+
+// 3
+// sum=(a,b)=>{
+//   console.log(( a*b));
+// }
+
+// sum(3,5)
+
+
+
+
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl ();
+
+
+
+// var x = 21;
+// a();
+// b();
+
+//   function a() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+//  function b() {
+    
+//     x = 40;
+//    console.log(x);
+// }
+
+
+
+
+// var x = 21;
+// a();
+// b();
+
+//   function a() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+//  function b() {
+    
+//     x = 40;
+//    console.log(x);
+// };
+// fun()// function fac(n){
+
+//     let fact=1;
+//     for(let i=n;i>=1;i--){
+//         fact=fact*i
+//     }
+//     return (fact)
+
+// }
+// fac(5)
+// console.log(fac(5));
